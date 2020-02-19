e1e01b6b84396511c7b23f7b29a26c98d13f4e70
kawsar-netizen <60083776+kawsar-netizen@users.noreply.github.com>
2020-02-20 02:58:30 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css" class="rel">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap">
+    <title>KhanTechnical Lmd.</title>
+</head>
+<body>
+    
+
+  
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">KhanTechnical Lmd.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="service.php">Services</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/k4.jpeg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Product</h3>
+        <p>Each product is sold at a price.</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/k2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+	   <h3>Gallery</h3>
+        <p>First look photos for khanTechnical</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/k3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Robot Testing</h3>
+        <p>It is a safe balance robot testing...</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+
+<div class="py-5">
+    <h2 class="text-center">About Us</h3>
+</div>
+<div class="container-fluid">
+	<div class="row">
+  <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/d5.jpg" class="img-fluid abouting" alt="pic">
+ </div>
+ <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">Our KhanTechnical</h2>
+	<p class="py-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis mauris vitae tincidunt imperdiet. Fusce bibendum enim varius, ultrices lorem quis, tempus risus. Ut sollicitudin leo ligula, at feugiat nisl dignissim eu.</p>
+	<a href="about.php" class="btn btn-primary"> Click me </a>
+</div>
+</div>
+</div>
+</section>
+
+<section class="my-5">
+
+<div class="py-5">
+    <h2 class="text-center">Our Services</h3>
+</div>
+<div class="container-fluid">
+
+   <div class="row">
+     <div class="col-lg-4 col-md-4 col-12">
+	 <div class="card k1">
+  <img class="card-img-top pic1" src="images/s1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Kawsar Khan</h4>
+    <p class="card-text">PHP coding developer</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+	 </div>
+	 <div class="col-lg-4 col-md-4 col-12">
+	 <div class="card k2">
+  <img class="card-img-top pic2" src="images/s1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Joe dome</h4>
+    <p class="card-text">Electrical site</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+	 </div>
+	 <div class="col-lg-4 col-md-4 col-12">
+	 <div class="card k3">
+  <img class="card-img-top pic3" src="images/s1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Al-Nafian</h4>
+    <p class="card-text">Design site</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+	 </div>
+   </div>
+   
+</div>
+
+</section>
+
+<section class="my-5">
+
+<div class="py-5">
+    <h2 class="text-center">Gallery</h3>
+</div>
+     <div class="container-fluid">
+       
+       <div class="row">
+         <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/k4.jpg" class="img-fluid pb-3 rounded" alt="DIU">
+           </div>
+         </div>
+		 <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/dia.jpg" class="img-fluid pb-3 rounded" alt="DIU">
+           </div>
+         </div>
+		 <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/k4.jpg" class="img-fluid pb-3 rounded" alt="DIU">
+           </div>
+         </div>
+		 <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/dia.jpg" class="img-fluid pb-3 rounded" alt="DIU">
+           </div>
+         </div>
+		 <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/k4.jpg" class="img-fluid pb-3 rounded" alt="DIU">
+           </div>
+         </div>
+		 <div class="col-md-4 col-lg-4 col-12">
+           <div>
+             <img src="images/dia.jpg" class="img-fluid rounded" alt="DIU">
+           </div>
+         </div>
+        
+        </div>
+      </div>
+     </div>
+</section>
+
+<section class="my-5">
+
+<div class="py-5">
+    <h2 class="text-center">Contact</h3>
+</div>
+<div class="w-50 m-auto">
+  <form action="userinfo.php" method="post">
+  
+  <div class="form-group">
+  <label>Username:</label>
+  <input type="text" name="user" autocomplete="off" class="form-control">
+  </div>
+  <div class="form-group">
+  <label>Email Id:</label>
+  <input type="text" name="email" autocomplete="off" class="form-control">
+  </div>
+  <div class="form-group">
+  <label>Mobile No:</label>
+  <input type="text" name="mobile" autocomplete="off" class="form-control">
+  </div>
+   <div class="form-group">
+  <label>Comments:</label>
+  <textarea class="form-control" name="comment">
+  </textarea>
+  </div>
+   <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+</section>
+
+
+<footer>
+   <p class="lead p-4 bg-dark text-white text-center">@KhanTechnicalProduction</p>
+</footer>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ service.php
@@ -0,0 +1,10 @@
+
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <h2 class="text-light text-center">Not Completed Section</h2>
+			  <p class="lead">Design By:Kawsar Khan Sizar</p>
+            </div>
+          </div>
+        </div>
+  
